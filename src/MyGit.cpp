79afdf642dd8c6e7cc4d69265094aa52e27a1eb1
Xiang Yang <xiang@vclass.com.au>
2015-01-15 17:38:38 +1100

    int max()
    get two numbers from console and return the bigger one

--- src/MyGit.cpp
+++ src/MyGit.cpp
@@ -11,18 +11,30 @@ using namespace std;
 
 //Declaration
 int sum();
+int max();
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	sum();
+	max();
 	return 0;
 }
 
 // get two numbers from console and return the sum
 int sum() {
 	int a, b, sum;
-	cin >> a>> b;
+	cin >> a >> b;
 	sum = a + b;
 	cout << "a+b=" << sum << endl;
 	return sum;
+}
+// get two numbers from console and return the bigger one
+int max() {
+	int a, b, m;
+	cin >> a >> b;
+	if (a > b)
+		m = a;
+	else
+		m = b;
+	cout << "max=" << m << endl;
+	return m;
 }
