1b0caa895e7ea2a87aa1f1ad6c684733b9ed379d
Xiang Yang <xiang@vclass.com.au>
2015-01-15 17:34:00 +1100

    int sum()
    get two numbers from console and return the sum

--- src/MyGit.cpp
+++ src/MyGit.cpp
@@ -9,7 +9,20 @@
 #include <iostream>
 using namespace std;
 
+//Declaration
+int sum();
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	sum();
 	return 0;
+}
+
+// get two numbers from console and return the sum
+int sum() {
+	int a, b, sum;
+	cin >> a>> b;
+	sum = a + b;
+	cout << "a+b=" << sum << endl;
+	return sum;
 }
